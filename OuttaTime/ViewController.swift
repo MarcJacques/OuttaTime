49405f35c53746a415e2781ea31563380922a208
Marc Jacques <Marcjacques@me.com>
2020-02-18 21:48:33 -0500

    Initial Commit

---
+++ OuttaTime/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  OuttaTime
+//
+//  Created by Marc Jacques on 2/18/20.
+//  Copyright © 2020 Marc Jacques. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
